6d06ec3ef5feef156d73995531592f1405a458aa
fatemanazari <fatemanazari78@gmail.com>
2024-01-16 22:12:48 +0330

    Create an API endpoint that selects a random greeting from the table

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,9 @@
+class GreetingsController < ApplicationController
+  def random_greeting
+    random_message = Message.order("RANDOM()").first
+    render json: { greeting: random_message&.content || 'No greetings available' }
+  end
+
+  def index
+  end
+end
